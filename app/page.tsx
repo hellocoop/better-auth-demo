6a5bd4dc21c96d1e194734781280afee14bc4501
rohanharikr <rohanharikumar80@gmail.com>
2025-10-09 10:05:48 +0100

    Refactor Home and SignInButton components to integrate Better Auth; update session handling and UI elements for improved user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-"use client";
+import Link from "next/link";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { SignInButton } from "@/components/sign-in-btn";
+import { Suspense } from "react";
+import { Button } from "@/components/ui/button";
 
-import { client } from "@/lib/auth-client";
-import { ContinueButton } from "@hellocoop/better-auth";
-import { useState } from "react";
-
-
-export default function Home() {
-	const [isLoading, setIsLoading] = useState(false);
+export default async function Home() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
 	return (
 		<div className="min-h-[80vh] flex items-center justify-center overflow-hidden no-visible-scrollbar px-6 md:px-0">
 			<main className="flex flex-col gap-4 row-start-2 items-center justify-center">
 				<div className="flex flex-col gap-1">
-					<h3 className="font-bold text-4xl text-black dark:text-white text-center">
-						Hello Better Auth Demo
+					<h3 className="font-bold text-3xl md:text-4xl text-black dark:text-white text-center">
+					Hellō Better Auth Demo
 					</h3>
 					<p className="text-center break-words text-sm md:text-base">
 					Next.js demo app showcasing the Better Auth plugin for{" "}
@@ -25,32 +27,39 @@ export default function Home() {
 						</a>{" "}
 					</p>
 				</div>
-				<div className="md:w-10/12 w-full flex flex-col gap-4">
-					{/* <div className="flex flex-col gap-3 pt-2 flex-wrap">
+				<div className="w-full flex flex-col">
+					<div className="text-left text-sm w-56 mx-auto opacity-50">
+						<span>You will be prompted to provide:</span>
+						<ul className="list-disc list-inside">
+							<li>Name</li>
+							<li>Verified Email</li>
+							<li>Profile Picture</li>
+						</ul>
+					</div>
+					{session?.session ? (
+						<Link href="/dashboard" className="mx-auto mt-6">
+						<Button className="gap-2 justify-between" variant="default">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="1.2em"
+								height="1.2em"
+								viewBox="0 0 24 24"
+							>
+								<path
+									fill="currentColor"
+									d="M5 3H3v4h2V5h14v14H5v-2H3v4h18V3zm12 8h-2V9h-2V7h-2v2h2v2H3v2h10v2h-2v2h2v-2h2v-2h2z"
+								></path>
+							</svg>
+							<span>Dashboard</span>
+						</Button></Link>) : <SignInButton/>
+					}
+					<div className="flex flex-col gap-3 pt-16 flex-wrap">
 						<div className="border-y py-2 border-dotted bg-secondary/60 opacity-80">
-							<div className="text-xs flex items-center gap-2 justify-center text-muted-foreground ">
-								<span className="text-center">
-									Placeholder text for now
-								</span>
+							<div className="text-xs text-center gap-2 text-muted-foreground">
+								This sample app demonstrates how you can add Hellō login and registration to Better Auth in seconds using the <a href="https://www.npmjs.com/package/@hellocoop/better-auth" className="inline underline">@hellocoop/better-auth</a> plugin package.
 							</div>
 						</div>
-					</div> */}
-					<ContinueButton 
-						className={`w-56 mx-auto mt-6 hello-btn-black-and-static hello-btn-hover-flare ${isLoading ? 'hello-btn-loader' : ''}`}
-						disabled={isLoading}
-						onClick={async () => {
-							setIsLoading(true);
-							try {
-								await (client as any).signInWithHello({
-									callbackURL: "/dashboard",
-								});
-							} catch (error) {
-								console.error('Sign in error:', error);
-								setIsLoading(false);
-							}
-						}} 
-					/>
-					
+					</div>
 				</div>
 			</main>
 		</div>
--- components/sign-in-btn.tsx
+++ components/sign-in-btn.tsx
@@ -1,87 +1,26 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+"use client";
 
-export async function SignInButton() {
-	const session = await auth.api.getSession({
-		headers: await headers(),
-	});
+import { client } from "@/lib/auth-client";
+import { ContinueButton } from "@hellocoop/better-auth";
+import { useState } from "react";
 
+export function SignInButton() {
+	const [isLoading, setIsLoading] = useState(false);
 	return (
-		<Link
-			href={session?.session ? "/dashboard" : "/sign-in"}
-			className="flex justify-center"
-		>
-			<Button className="gap-2  justify-between" variant="default">
-				{!session?.session ? (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="1.2em"
-						height="1.2em"
-						viewBox="0 0 24 24"
-					>
-						<path
-							fill="currentColor"
-							d="M5 3H3v4h2V5h14v14H5v-2H3v4h18V3zm12 8h-2V9h-2V7h-2v2h2v2H3v2h10v2h-2v2h2v-2h2v-2h2z"
-						></path>
-					</svg>
-				) : (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="1.2em"
-						height="1.2em"
-						viewBox="0 0 24 24"
-					>
-						<path fill="currentColor" d="M2 3h20v18H2zm18 16V7H4v12z"></path>
-					</svg>
-				)}
-				<span>{session?.session ? "Dashboard" : "Sign In"}</span>
-			</Button>
-		</Link>
-	);
-}
-
-function checkOptimisticSession(headers: Headers) {
-	const guessIsSignIn =
-		headers.get("cookie")?.includes("better-auth.session") ||
-		headers.get("cookie")?.includes("__Secure-better-auth.session-token");
-	return !!guessIsSignIn;
-}
-
-export async function SignInFallback() {
-	//to avoid flash of unauthenticated state
-	const guessIsSignIn = checkOptimisticSession(await headers());
-	return (
-		<Link
-			href={guessIsSignIn ? "/dashboard" : "/sign-in"}
-			className="flex justify-center"
-		>
-			<Button className="gap-2  justify-between" variant="default">
-				{!guessIsSignIn ? (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="1.2em"
-						height="1.2em"
-						viewBox="0 0 24 24"
-					>
-						<path
-							fill="currentColor"
-							d="M5 3H3v4h2V5h14v14H5v-2H3v4h18V3zm12 8h-2V9h-2V7h-2v2h2v2H3v2h10v2h-2v2h2v-2h2v-2h2z"
-						></path>
-					</svg>
-				) : (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="1.2em"
-						height="1.2em"
-						viewBox="0 0 24 24"
-					>
-						<path fill="currentColor" d="M2 3h20v18H2zm18 16V7H4v12z"></path>
-					</svg>
-				)}
-				<span>{guessIsSignIn ? "Dashboard" : "Sign In"}</span>
-			</Button>
-		</Link>
+		<ContinueButton 
+			className={`w-56 mt-6 mx-auto hello-btn-black-and-static hello-btn-hover-flare ${isLoading ? 'hello-btn-loader' : ''}`}
+			disabled={isLoading}
+			onClick={async () => {
+				setIsLoading(true);
+				try {
+					await (client as any).signInWithHello({
+						callbackURL: "/dashboard",
+					});
+				} catch (error) {
+					console.error('Sign in error:', error);
+					setIsLoading(false);
+				}
+			}} 
+		/>
 	);
 }
